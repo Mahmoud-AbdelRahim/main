bfa4c9c2778be520f34c1c15c990be9e0fe4c6fb
Mahmoud-AbdelRahim <mahmmoud.abd.elrahim@gmail.com>
2023-09-25 21:01:22 +0300

    Editing script.groovy

--- script.groovy
+++ script.groovy
@@ -1,17 +1,25 @@
 def TestApp () {
   echo "Testing The App"
   echo "Executing pipeline into branch $BRANCH_NAME"
+
+  sh 'cd /var/jenkins_home/workspace/nodejs-multibranch-pipeline_main/'
+  sh 'npm install'
+  sh 'npm test'
 }
 
 def BuildApp () {
   echo "Building the App from branch $BRANCH_NAME"
   sh 'cd /var/jenkins_home/workspace/nodejs-multibranch-pipeline_main/'
 //  sh 'npx ci'
-  sh 'npx turbo serve'
+//  sh 'npx turbo serve'
+    sh 'npm run serve'
 }
 
 def DeployApp () {
   echo "Deploying Branch $BRANCH_NAME"
+  //sh 'docker build -t NodeJs-App:1.0 /var/jenkins_home/workspace/nodejs-multibranch-pipeline_main'
+
+
 }
 
 return this
